fa7c99ac0d9b62e159cbefa31c525d4fdbfabd6e
Justin Hsieh <showin@live.com>
2019-09-03 15:59:34 -0700

    commit

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 import setuptools
 
+REQUIRED = [
+    "numpy",
+    "pandas"
+]
+
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="lambdata-jhsieh",
+    name="lambdatajhsieh",
     version="0.0.1",
     author="jhsieh",
     author_email="",
@@ -13,6 +18,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/justin-hsieh/lambdata",
     packages=setuptools.find_packages(),
+    install_requires=REQUIRED,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
