a344e92b5b05023b661728f7a7d4434eb880e320
Justin Hsieh <showin@live.com>
2019-09-03 19:43:41 -0700

    commit

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="lambdatahsieh",
-    version="0.0.2",
+    version="0.1.2",
     author="jhsieh",
     description="A small example package",
     long_description=long_description,
