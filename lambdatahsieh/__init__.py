df84e548928d45e3a13d988b26d3373f87f26fef
Justin Hsieh <showin@live.com>
2019-09-03 18:52:45 -0700

    commit

--- lambdatahsieh/__init__.py
+++ lambdatahsieh/__init__.py
@@ -1,15 +1,8 @@
+
+
+
+
 import numpy as np
 import pandas as pd
 
-ONES =  pd.DataFrame(np.one(10))
-# Your init script
-#
-# Atom will evaluate this file each time a new window is opened. It is run
-# after packages are loaded/activated and after the previous editor state
-# has been restored.
-#
-# An example hack to log to the console when each text editor is saved.
-#
-# atom.workspace.observeTextEditors (editor) ->
-#   editor.onDidSave ->
-#     console.log "Saved! #{editor.getPath()}"
+ONES =  pd.DataFrame(np.ones(10))
